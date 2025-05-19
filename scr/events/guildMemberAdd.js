803812620b4584c9ffc50ef0b5c102896400b9b0
Zynix <162629879+ZynixDevelopment@users.noreply.github.com>
2025-05-19 15:17:15 +0200

    Delete scr directory

--- scr/events/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'guildMemberAdd',
-  execute(member) {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'général');
-    if (channel) {
-      channel.send(`Bienvenue ${member} sur **${member.guild.name}** !`);
-    }
-  }
-};
--- scr/events/ready.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(chalk.cyanBright(`Bot prêt - ${client.user.tag}`));
-  }
-};
