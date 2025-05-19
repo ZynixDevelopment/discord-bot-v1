2f0a4db102b81cb029c42f288c126add5dd3b370
Zynix <162629879+ZynixDevelopment@users.noreply.github.com>
2025-05-19 21:03:56 +0200

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
   name: 'c',
   execute(member) {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'général');
     if (channel) {
-      channel.send(`Bienvenue ${member} sur **${member.guild.name}** !`);
+      channel.send(`👋 Bienvenue ${member} sur **${member.guild.name}** !`);
     }
   }
 };
