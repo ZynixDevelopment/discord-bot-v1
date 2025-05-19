41d707cd3936a0b311a060ca5da52d68a5e509a3
Zynix <162629879+ZynixDevelopment@users.noreply.github.com>
2025-05-19 21:06:11 +0200

    Update userinfo.js

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,11 +1,12 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
-module.exports = {
+export default {
   data: new SlashCommandBuilder()
     .setName('userinfo')
     .setDescription('Affiche des informations sur un utilisateur')
     .addUserOption(option =>
       option.setName('utilisateur').setDescription('Utilisateur à inspecter')),
+
   async execute(interaction) {
     const user = interaction.options.getUser('utilisateur') || interaction.user;
     const member = await interaction.guild.members.fetch(user.id);
@@ -14,11 +15,12 @@ module.exports = {
       .setTitle(`Informations de ${user.tag}`)
       .setThumbnail(user.displayAvatarURL())
       .addFields(
-        { name: 'Pseudo', value: user.username, inline: true },
-        { name: 'ID', value: user.id, inline: true },
-        { name: 'A rejoint le', value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:D>`, inline: true }
+        { name: '👤 Pseudo', value: user.username, inline: true },
+        { name: '🆔 ID', value: user.id, inline: true },
+        { name: '📅 A rejoint le', value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:D>`, inline: true }
       )
       .setColor(0x3498db);
+
     await interaction.reply({ embeds: [embed] });
   }
 };
